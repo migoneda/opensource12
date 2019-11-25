e1452cf5bb30512e6d95fd76c465d77453bac3a2
leechanhoe <tkddmsdl34@naver.com>
2019-11-25 15:27:01 +0900

    added spi

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import spidev
 
 # 모터 드라이버 연결 PIN
 A1A = 5
@@ -14,3 +15,13 @@ GPIO.setup(A1A, GPIO.OUT)
 GPIO.setup(A1A, GPIO.LOW)
 GPIO.setup(A1B, GPIO.OUT)
 GPIO.output(A1B, GPIO.LOW)
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 500000
+# ADC값을 가져오는 함수
+def read_spi_adc(adcChannel):
+	adcValue = 0
+	buff = spi.xfer2([1,(8+adcChannel)<<4, 9])
+	adcValue = ((buff[1]&3)<<8)+buff[2]
+	return adcValue
